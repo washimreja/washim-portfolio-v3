950f5d8497eafca91c96d2a8b12ac169757b0205
Washim Reja <wreja2030@gmail.com>
2026-04-19 10:29:38 +0530

    fix local and pages preview paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/washim-portfolio-v3/",
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "");
+
+  return {
+    base: env.VITE_BASE_PATH || "/",
+    plugins: [react()],
+  };
 });
